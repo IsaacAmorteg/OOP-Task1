12d8f2f749cecc3af16f89fd0a6cd882bc8f0b9e
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-19 15:23:41 -0500

    Square class created

---
+++ Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryClasses
+{
+    class Square : Shape
+    {
+        private double _sideLenght;
+
+        public Square(double sideLenght) 
+        {
+            _sideLenght = sideLenght;
+        }
+        public override double GetArea()
+        {
+            return _sideLenght * _sideLenght;
+        }
+
+        public override double GetPerimeter()
+        {
+            return 4 * _sideLenght;
+        }
+    }
+}
