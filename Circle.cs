0ce1f4aa6ef37ec433cafe58455d08533abcd26a
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-19 15:50:57 -0500

    Creation of Circle class

---
+++ Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryClasses
+{
+    class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(double radius) 
+        {
+            _radius = radius;
+        }
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * _radius;
+        }
+    }
+ 
+}
